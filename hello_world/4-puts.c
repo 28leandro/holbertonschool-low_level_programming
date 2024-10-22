8385a516c2cfda148b9b0b817ec5e01c60c44e16
28leandro <andrelearde@hotmail.com>
2024-10-22 13:52:29 +0200

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
 /**
- * 
+ * main - Entry point
  *
+ * Return: ALways 0
  */
-int main (void){ :
-puts("Programming is like building a multilingual puzzle");
-return 0; // Ending the program with the value 0
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
