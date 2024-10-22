18fd34324c5a700ad977742b123f514516e5cb19
28leandro <andrelearde@hotmail.com>
2024-10-22 11:01:59 +0200

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -10,10 +10,6 @@ int main(void) {
 puts("Programming is like building a multilingual puzzle");
     return 0;
 }#include <stdio.h>
-
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    
  
 return 0; // Ending the program with the value 0
 }
